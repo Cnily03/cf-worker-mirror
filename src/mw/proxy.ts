02cd7540a024ffdec71fbea11d2a8287179a1fa3
Cnily03 <cnily03@outlook.com>
2026-08-03 23:29:05 +0800

    :sparkles: redirect with the same origin as the client

--- src/mw/proxy.ts
+++ src/mw/proxy.ts
@@ -149,7 +149,7 @@ export function forwardPath<T extends Env = BlankEnv>(options?: Partial<ProxyFor
                 location = `${forwardUrlObj.origin}${location}`
             }
             headers.delete('Location')
-            headers.set('Location', new URL(thisUrlObj.origin + '/' + location).href)
+            headers.set('Location', new URL(/* thisUrlObj.origin + */ '/' + location).href)
         }
 
         return new Response(resp.body, {
